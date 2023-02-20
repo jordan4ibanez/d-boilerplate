ae39c277bdc464ddaf052541dc52ea7f03678742
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-02-20 00:08:31 -0500

    Simplify

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,8 @@ export function activate(context: vscode.ExtensionContext) {
     let gettersAndSetters = vscode.commands.registerCommand("d-boilerplate.addGettersSetters", () => {
         console.log("hello :)");
     });
+    
     context.subscriptions.push(gettersAndSetters);
-
-    let getters = vscode.commands.registerCommand("d-boilerplate.addGettersSetters", () => {
-        console.log("hello :)");
-    });
-    context.subscriptions.push(getters);
-
-    let setters = vscode.commands.registerCommand("d-boilerplate.addGettersSetters", () => {
-        console.log("hello :)");
-    });
-    context.subscriptions.push(setters);
 
 
 }
